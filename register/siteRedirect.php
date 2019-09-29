6c2edfd61fd67267b2cac1fbf769440200451ccf
yash <yash17.ys4@gmail.com>
2019-09-29 11:48:09 +0530

    website added

---
+++ register/siteRedirect.php
@@ -0,0 +1,20 @@
+
+
+
+<!-- Redirecting Statement -->
+<div style="text-align: center;">
+	<img src="loader.gif" />
+	<h3>Please don't refresh or reload the page.</h3>
+	<h3>You are being redirected...</h3>
+</div>
+
+
+
+<?php
+	
+	include('registrationMail.php');
+
+	// header("Location: http://localhost:8080/");
+	header("Location: https://srijaniitism.org/");
+
+?>
